6177f8d171db066991001d357bf1300c7635c342
alex <canghai187@163.com>
2015-12-23 15:37:46 +0800

    add Class DoubleNode

---
+++ chapertwo/DoubleNode.java
@@ -0,0 +1,12 @@
+package chapertwo;
+
+public class DoubleNode {
+    public DoubleNode prev;
+    public DoubleNode next;
+    public int value;
+    public DoubleNode(int value) {
+        super();
+        this.value = value;
+    }
+    
+}
